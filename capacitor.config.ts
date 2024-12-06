df487e80b7c7e384e1fc38616d74466cc8ce98b7
marcosborg <m.borges.mail@gmail.com>
2024-12-06 10:52:48 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mundo.tvde',
+  appName: 'mundo-tvde',
+  webDir: 'www'
+};
+
+export default config;
